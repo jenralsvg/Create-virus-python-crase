15b804670ce7defc44098f327d7bbc3ab80b0f6a
FERDI Z-afk <69185890+FERDIZ-afk@users.noreply.github.com>
2021-02-23 17:00:44 +0700

    update herokututor.py

--- herokututor.py
+++ herokututor.py
@@ -18,22 +18,104 @@ def convert_size(size_bytes):
 	s = round(size_bytes / p, 2)
 	return '%s %s' % (s, size_name[i])
 	
-	
+@app.route('/api/ytv', methods=['GET','POST'])
+def ytv():
+	if request.args.get('url'):
+		try:
+			url = request.args.get('url').replace('[','').replace(']','')
+			ytv = post('https://www.y2mate.com/mates/en60/analyze/ajax',data={'url':url,'q_auto':'0','ajax':'1'}).json()
+			yaha = bs(ytv['result'], 'html.parser').findAll('td')
+			filesize = yaha[len(yaha)-23].text
+			id = re.findall('var k__id = "(.*?)"', ytv['result'])
+			thumb = bs(ytv['result'], 'html.parser').find('img')['src']
+			title = bs(ytv['result'], 'html.parser').find('b').text
+			dl_link = bs(post('https://www.y2mate.com/mates/en60/convert',data={'type':url.split('/')[2],'_id':id[0],'v_id':url.split('/')[3],'ajax':'1','token':'','ftype':'mp4','fquality':'360p'}).json()['result'],'html.parser').find('a')['href']
+			return {
+				'status': 200,
+				'restapi' : 'ferdiz-afk',
+				'title': title,
+				'thumb': thumb,
+				'result': dl_link,
+				'resolution': '360p',
+				'filesize': filesize,
+				'ext': 'mp4'
+			}
+		except Exception as e:
+			print('Error : %s ' % e)
+			return {
+				'status': False,
+				'error': '[❗] Terjadi kesalahan, mungkin link yang anda kirim tidak valid!'
+			}
+	else:
+		return {
+			'status': False,
+			'msg': 'Masukkan parameter url'
+		}
 
+@app.route('/api/yta', methods=['GET','POST'])
+def yta():
+	if request.args.get('url'):
+		try:
+			url = request.args.get('url').replace('[','').replace(']','')
+			yta = post('https://www.y2mate.com/mates/en60/analyze/ajax',data={'url':url,'q_auto':'0','ajax':'1'}).json()
+			yaha = bs(yta['result'], 'html.parser').findAll('td')
+			filesize = yaha[len(yaha)-10].text
+			id = re.findall('var k__id = "(.*?)"', yta['result'])
+			thumb = bs(yta['result'], 'html.parser').find('img')['src']
+			title = bs(yta['result'], 'html.parser').find('b').text
+			dl_link = bs(post('https://www.y2mate.com/mates/en60/convert',data={'type':url.split('/')[2],'_id':id[0],'v_id':url.split('/')[3],'ajax':'1','token':'','ftype':'mp3','fquality':'128'}).json()['result'],'html.parser').find('a')['href']
+			return {
+				'status': 200,
+				'restapi' : 'ferdiz-afk',
+				'scrap' :  'youtube',
+				'title': title,
+				'thumb': thumb,
+				'filesize': filesize,
+				'result': dl_link,
+				'ext': 'mp3'
+			}
+		except Exception as e:
+			print('Error : %s' % e)
+			return {
+				'status': False,
+				'error': '[❗] Terjadi kesalahan mungkin link yang anda kirim tidak valid!'
+			}
+	else:
+		return {
+			'status': False,
+			'msg': '[!] Masukkan parameter url'
+		}
+	
+@app.route('/api/waifu', methods=['GET','POST'])
+def waifu():
+	scrap = bs(get('https://mywaifulist.moe/random').text, 'html.parser')
+	a = json.loads(scrap.find('script', attrs={'type':'application/ld+json'}).string)
+	desc = bs(get(a['url']).text, 'html.parser').find('meta', attrs={'property':'og:description'}).attrs['content']
+	result = json.loads(bs(get(a['url']).text, 'html.parser').find('script', attrs={'type':'application/ld+json'}).string)
+	if result['gender'] == 'female':
+		return {
+			'status': 200,
+			'restapi' : 'ferdiz-afk',
+			'name': result['name'],
+			'desc': desc,
+			'image': result['image'],
+			'source': result['url']
+		}
+	else:
+		return {
+			'status': 200,
+			'name': '%s (husbu)' % result['name'],
+			'desc': desc,
+			'image': result['image'],
+			'source': result['url']
+		}
 
 @app.route('/')
 def home():
     """Landing page."""
     return render_template('FILE-HTML-LO.html')
 
-@app.route("/api/<path:nazri>", methods=['GET', 'POST'])
-def api(nazri):
-    if nazri == "nazri":
-        ret = "Nazri Ganteng"
-        return make_response(jsonify(ret))
-    elif nazri == "tambah-aja-scraplo-sini":
-        ret["result"] = "Nazri Ganteng Cuy"
-        return make_response(jsonify(ret))
+
       
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=int(os.environ.get('PORT','5000')),debug=True)
