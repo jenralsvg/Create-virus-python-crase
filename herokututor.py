b57d005af2b5a7a5b9392eff43287ac550d52e5f
FERDI Z-afk <69185890+FERDIZ-afk@users.noreply.github.com>
2021-02-23 16:57:34 +0700

    update herokututor.py

--- herokututor.py
+++ herokututor.py
@@ -1,10 +1,25 @@
-from flask import Flask, Response, jsonify, abort, make_response,  render_template, redirect, request
-from faster_than_requests import requests
-from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
-import json, requests, traceback, os, lxml, re, urllib, urllib3, urllib.parse, argparse, pytz, urllib.request, time
+from urllib.parse import *
+from flask import *
+#from werkzeug.utils import *
+from bs4 import BeautifulSoup as bs
+from requests import get, post
+import os, math, json, random, re, html_text, pytesseract, base64, time, smtplib
+
+ua_ig = 'Mozilla/5.0 (Linux; Android 9; SM-A102U Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Instagram 155.0.0.37.107 Android (28/9; 320dpi; 720x1468; samsung; SM-A102U; a10e; exynos7885; en_US; 239490550)'
 
 app = Flask(__name__)
+
+def convert_size(size_bytes):
+	if size_bytes == 0:
+		return '0B'
+	size_name = ('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+	i = int(math.floor(math.log(size_bytes, 1024)))
+	p = math.pow(1024, i)
+	s = round(size_bytes / p, 2)
+	return '%s %s' % (s, size_name[i])
+	
+	
+
 
 @app.route('/')
 def home():
@@ -20,5 +35,5 @@ def api(nazri):
         ret["result"] = "Nazri Ganteng Cuy"
         return make_response(jsonify(ret))
       
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=int(os.environ.get('PORT','5000')),debug=True)
